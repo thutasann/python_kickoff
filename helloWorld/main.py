d8c4861ccba63ecff4a6574137f92ee8f279a3d0
thutasann <thutasann2002@gmail.com>
2024-07-21 19:39:06 +0800

    add: string_methods

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -1,2 +1,3 @@
 from src.variables.variables import *
 from src.variables.multiple_assignment import *
+from src.string_methods.string_methods import *
---
+++ helloWorld/src/string_methods/string_methods.py
@@ -0,0 +1,14 @@
+print("---------------- STRING METHODS ---------------- ")
+
+name = "Thuta Sann"
+
+print(len(name))
+print(name.find("S"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("n"))
+print(name.replace("S", "$"))
+print(name * 3)
