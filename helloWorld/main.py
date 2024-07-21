f85d45fda46c20ae39a063fb0c17d96178b0fb11
thutasann <thutasann2002@gmail.com>
2024-07-21 21:08:27 +0800

    add: type_casting

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -1,3 +1,4 @@
 from src.variables.variables import *
 from src.variables.multiple_assignment import *
 from src.string_methods.string_methods import *
+from src.type_casting.type_casting import *
---
+++ helloWorld/src/type_casting/type_casting.py
@@ -0,0 +1,13 @@
+print("---------------- TYPE CASTING ---------------- ")
+
+x = 1
+y = 2.0
+z = "3"
+
+x = float(x)
+y = int(y)
+z = int(z)
+
+print("x is " + str(x))
+print("y is " + str(y))
+print(z*3)
