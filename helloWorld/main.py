2c9c4d069645129859b140b10931b2070031c384
thutasann <thutasann2002@gmail.com>
2024-07-21 21:34:17 +0800

    remove: user_input from main

--- helloWorld/main.py
+++ helloWorld/main.py
@@ -2,5 +2,4 @@ from src.variables.variables import *
 from src.variables.multiple_assignment import *
 from src.string_methods.string_methods import *
 from src.type_casting.type_casting import *
-from src.user_input.user_input import *
 from src.math_functions.math_functions import *
